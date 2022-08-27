feca819a8515bfea4fa8af72c6807581c1594fec
Mateusz Krajewski <maatsior@gmail.com>
2022-08-27 16:37:08 +0200

    Add CommandLineRunner

--- src/main/java/com/matsior/api/StudentApiApplication.java
+++ src/main/java/com/matsior/api/StudentApiApplication.java
@@ -1,7 +1,18 @@
 package com.matsior.api;
 
+import com.matsior.api.model.Address;
+import com.matsior.api.model.Gender;
+import com.matsior.api.model.Student;
+import com.matsior.api.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class StudentApiApplication {
@@ -10,4 +21,25 @@ public class StudentApiApplication {
         SpringApplication.run(StudentApiApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner(StudentRepository repository) {
+        return args -> {
+            Address address = new Address(
+                    "Poland",
+                    "Gdynia",
+                    "81-198"
+            );
+            Student student = new Student(
+                    "John",
+                    "Doe",
+                    "john@doe.com",
+                    Gender.MALE,
+                    address,
+                    List.of("Computer Science", "Maths"),
+                    BigDecimal.TEN,
+                    LocalDateTime.now()
+            );
+            repository.save(student);
+        };
+    }
 }
